40549b4e5241e747f5850652d7b576a3e06915f8
蓝科 <lanke@lankeshichuan.local>
2016-05-17 19:51:27 +0800

    Initial Commit

---
+++ SCMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCMusic
+//
+//  Created by 蓝科 on 16/5/17.
+//  Copyright © 2016年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCMusic
+//
+//  Created by 蓝科 on 16/5/17.
+//  Copyright © 2016年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
